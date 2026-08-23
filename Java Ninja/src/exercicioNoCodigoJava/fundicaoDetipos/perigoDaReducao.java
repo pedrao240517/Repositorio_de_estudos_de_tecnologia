bb38e7b5a23fd8c01000d6aa6110e49f1137e923
Pedro Lucas <pedrao240517@gmail.com>
2026-08-23 12:16:16 -0400

    .

--- Java Ninja/src/exercicioNoCodigoJava/fundicaoDetipos/perigoDaReducao.java
+++ Java Ninja/src/exercicioNoCodigoJava/fundicaoDetipos/perigoDaReducao.java
@@ -1,4 +1,12 @@
 package exercicioNoCodigoJava.fundicaoDetipos;
 
 public class perigoDaReducao {
+    static void main(String[] args) {
+
+         /*double valor = 9.00;
+         int valorConvertido = valor;*/
+         //java: incompatible types: possible lossy conversion from double to int
+
+
+    }
 }
--- Java Ninja/src/exercicioNoCodigoJava/sintaxebasica/cadastrosimples.java
+++ Java Ninja/src/exercicioNoCodigoJava/sintaxebasica/cadastrosimples.java
@@ -1,4 +1,28 @@
 package exercicioNoCodigoJava.sintaxebasica;
 
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Scanner;
+
 public class cadastrosimples {
+    static void main(String[] args) {
+
+        System.out.println("=============SISTEMA DE CADASTRO==================");
+
+        // Scanner
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite seu nome: ");
+        String nome = sc.nextLine();
+        System.out.println("Digite sua idade: ");
+        int idade = sc.nextInt();
+        System.out.println("Digite sua altura(com,): ");
+        float altura = sc.nextFloat();
+        sc.close();
+        System.out.println("Cadastro realizado com sucesso!");
+
+
+
+
+
+    }
 }
