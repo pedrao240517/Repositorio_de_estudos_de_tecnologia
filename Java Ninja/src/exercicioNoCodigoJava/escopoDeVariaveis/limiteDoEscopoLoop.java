089166b023050564956f97e7a30c0440fe803c7a
Pedro-Lucas-Silva <pedrao240517@gmail.com>
2026-08-24 18:51:10 -0400

    .

--- Java Ninja/src/exercicioNoCodigoJava/escopoDeVariaveis/limiteDoEscopoLoop.java
+++ Java Ninja/src/exercicioNoCodigoJava/escopoDeVariaveis/limiteDoEscopoLoop.java
@@ -1,4 +1,16 @@
 package exercicioNoCodigoJava.escopoDeVariaveis;
 
 public class limiteDoEscopoLoop {
+    static void main(String[] args) {
+
+        for (int i = 0; i <10 ; i++) {
+            int dobro = 1 * 2;
+            System.out.println(dobro);
+        }
+       /* System.out.println(dobro); - variavel existe apenas no escopo do loop*/
+
+
+
+
+    }
 }
