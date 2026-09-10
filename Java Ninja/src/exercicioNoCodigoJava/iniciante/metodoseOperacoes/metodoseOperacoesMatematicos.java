3003a64d5bcbda1f71527acafeaa288a4cd2cedf
Pedro Lucas <pedrao240517@gmail.com>
2026-09-10 17:32:53 -0400

    .

--- Java Ninja/src/exercicioNoCodigoJava/iniciante/metodoseOperacoes/metodoseOperacoesMatematicos.java
+++ Java Ninja/src/exercicioNoCodigoJava/iniciante/metodoseOperacoes/metodoseOperacoesMatematicos.java
@@ -1,4 +1,66 @@
 package exercicioNoCodigoJava.iniciante.metodoseOperacoes;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class metodoseOperacoesMatematicos {
+    static void main(String[] args) {
+
+        int escolha;
+
+        System.out.println("-----------------CALCULADORA NUMEROS INTEIROS---------------------");
+        System.out.println("1-soma");
+        System.out.println("2- subtração");
+        System.out.println("3-Multiplicacao");
+        System.out.println("4-Divisão");
+        System.out.println("Escolha a operação");
+        Scanner input = new Scanner(System.in);
+        input.nextInt();
+        escolha = input.nextInt();
+
+        while (escolha != 5){
+            int numero1;
+            int numero2;
+            int resultado;
+            switch (escolha){
+                case 1:
+                    System.out.println("Digite o primeiro número");
+                    numero1 = input.nextInt();
+                    System.out.println("Digite o segundo número");
+                    numero2 = input.nextInt();
+                     resultado= numero1 + numero2;
+                    break;
+                case 2:
+                    System.out.println("Digite o primeiro número");
+                    numero1 = input.nextInt();
+                    System.out.println("Digite o segundo número");
+                    numero2 = input.nextInt();
+                    resultado= numero1 - numero2;
+                    break;
+                case 3:
+                    System.out.println("Digite o primeiro número");
+                    numero1 = input.nextInt();
+                    System.out.println("Digite o segundo número");
+                    numero2 = input.nextInt();
+                    resultado= numero1 * numero2;
+                    break;
+                case 4:
+                    System.out.println("Digite o primeiro número");
+                    numero1 = input.nextInt();
+                    System.out.println("Digite o segundo número");
+                    numero2 = input.nextInt();
+                    resultado= numero1 / numero2;
+                    break;
+                case 5:
+                    System.out.println("Saindo do programa ");
+                default:
+                    System.out.println("número inválido");
+                    break;
+            }
+        }
+        input.close();
+
+
+
+    }
 }
